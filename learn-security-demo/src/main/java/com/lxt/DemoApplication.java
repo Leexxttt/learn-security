aa1cb778f7eb2f89da35d8dcfdfec96be8e123f9
reallyCommon <871281347@qq.com>
2018-01-01 18:58:19 +0800

    Initial commit

---
+++ learn-security-demo/src/main/java/com/lxt/DemoApplication.java
@@ -0,0 +1,30 @@
+/*****************************************************
+ * Description:
+ *
+ *@auther:李雪腾
+ *@date：2018-1-1
+ *****************************************************/
+package com.lxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Administrator
+ *
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String hello(){
+		return "你好,spring security!";
+	}
+}
